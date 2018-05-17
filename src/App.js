664e67dcc95c12265c3424263b4763909b51bd06
Carl Svahnstrom <carl@tocco.se>
2018-05-17 13:49:21 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -2,41 +2,41 @@ import React, {Component} from 'react';
 import IceCream from './Components/IceCream';
 import './App.css';
 
-const iceCream = [
-  {
-    flavour: 'Chocolate',
-    nuts: 'Plenty of nuts',
-    price: 25,
-    palette: 'brown'
-  },
-  {
-    flavour: 'Vanilla',
-    nuts: 'No nuts',
-    price: 25,
-    palette: 'yellow'
-  },
-  {
-    flavour: 'Bubble-Gum',
-    nuts: 'No nuts',
-    price: 20,
-    palette: 'pink'
-  },
-  {
-    flavour: 'Pear',
-    nuts: 'Some nuts',
-    price: 25,
-    palette: 'green'
-  },
-  {
-    flavour: 'Orange dawn',
-    nuts: 'Crazy much nuts',
-    price: 23,
-    palette: 'orange'
-  }
-
-];
-
-localStorage.iceCream = JSON.stringify(iceCream);
+// const iceCream = [
+//   {
+//     flavour: 'Chocolate',
+//     nuts: 'Plenty of nuts',
+//     price: 25,
+//     palette: 'brown'
+//   },
+//   {
+//     flavour: 'Vanilla',
+//     nuts: 'No nuts',
+//     price: 25,
+//     palette: 'yellow'
+//   },
+//   {
+//     flavour: 'Bubble-Gum',
+//     nuts: 'No nuts',
+//     price: 20,
+//     palette: 'pink'
+//   },
+//   {
+//     flavour: 'Pear',
+//     nuts: 'Some nuts',
+//     price: 25,
+//     palette: 'green'
+//   },
+//   {
+//     flavour: 'Orange dawn',
+//     nuts: 'Crazy much nuts',
+//     price: 23,
+//     palette: 'orange'
+//   }
+//
+// ];
+//
+// localStorage.iceCream = JSON.stringify(iceCream);
 
 class App extends Component {
   constructor() {
@@ -55,11 +55,13 @@ deleteIceCream(flavour) {
   this.setState ({
     iceCream: filteredIceCream
   })
+  localStorage.iceCream = JSON.stringify(filteredIceCream);
+  console.log(JSON.parse(localStorage.iceCream))
 }
 
 onSubmitHandler(e) {
   e.preventDefault();
-  
+
   let iceCream = this.state.iceCream;
 
   iceCream.push({
@@ -70,8 +72,9 @@ onSubmitHandler(e) {
   })
   this.setState ({
     iceCream: iceCream
-
   })
+  localStorage.iceCream = JSON.stringify(iceCream);
+  console.log(localStorage.iceCream)
 }
 
   render() {
