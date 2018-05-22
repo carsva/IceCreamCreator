6e3338d8a8121e1b6e0dfa73a7e554841b9f25df
Carl Svahnstrom <carl@tocco.se>
2018-05-22 15:08:02 +0200

    Added new icecream with two colors, fixed broken css

--- src/App.js
+++ src/App.js
@@ -131,20 +131,24 @@ this.setState({
          fill: this.state.background
        };
 
+
+
     return (
-      <div className="App">
+
+      <div className="container-full">
         {iceCreamMap}
-        <form onSubmit={this.onSubmitHandler.bind(this)}>
+        <form className="formwrapper" onSubmit={this.onSubmitHandler.bind(this)}>
           <input type="text" placeholder="Flavour" ref="flavour"/>
           <input type="text" placeholder="Nuts" ref="nuts"/>
           <input type="text" placeholder="Price" ref="price"/>
-          <input type="color" onChange={this.onChangeColor.bind(this)} ref="color" />
-          <button className="btn btn-primary">Create Ice cream</button>
+          <input className="picker" type="color" onChange={this.onChangeColor.bind(this)} ref="color" />
+          <br /><button className="btn btn-primary">Create Ice cream</button>
         </form>
-
-        <svg className="show" id="Lager_1" data-name="Lager 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 185.78 218.1"><path style={style} d="M121.43,236.55a192.5,192.5,0,0,0-30.78-78.38,194.51,194.51,0,0,0-26.37-31.12c-10.49-10.08-22.17-20-26.49-34.41l-1.44,1.9,182.81,1.72L218.1,93.7c-37.93,44.71-61.5,99.54-99.79,144-1.26,1.46.85,3.59,2.12,2.12,38.3-44.43,61.87-99.26,99.79-144a1.52,1.52,0,0,0-1.06-2.56L36.35,91.54a1.52,1.52,0,0,0-1.45,1.9c3.85,12.87,13.22,22.38,22.72,31.42,10.47,10,20.26,20.29,28.59,32.15a190.71,190.71,0,0,1,32.32,80.34c.3,1.9,3.19,1.09,2.9-.8Z" transform="translate(-34.84 -22.19)"/><path style={style} d="M50.81,91.8C38.48,81.65,38.62,63.52,47.37,51.1c9.8-13.92,26.65-20,42.79-22.65C123.23,22.93,163,21.59,191,43c15,11.46,28.22,32.81,16.6,51.1-1,1.63,1.56,3.14,2.59,1.51,10-15.67,2.95-34.48-8.65-47.08-11.92-13-28.74-20.6-45.82-23.89-20.57-4-42.23-2.58-62.84.39-17.43,2.5-35.6,8.31-46.8,22.78C35.59,61.37,34.6,82.32,48.68,93.92c1.48,1.22,3.62-.89,2.13-2.12Z" transform="translate(-34.84 -22.19)"/>
+        <div className="formwrapper">
+        <svg className="show" id="Lager_1" data-name="Lager 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 182.82 214.94"><path className="kex" d="M120,237A190.07,190.07,0,0,0,65.45,130.27C54.05,119.1,40.61,108.42,36.35,93l182.81,1.72c-37.95,44.46-61.49,99.81-99.79,144" transform="translate(-36.35 -23.79)"/>
+        <path style={style} d="M49.74,92.86c-9.86-7.65-12.55-22.31-8.19-34s14.61-20.47,26-25.55,24-6.88,36.38-8.1c18-1.78,36.44-2.45,54.15,1.43S192.94,39.35,204,53.72a44.84,44.84,0,0,1,9.15,19.92c1.18,7.29-.06,15.1-4.22,21.2" transform="translate(-36.35 -23.79)"/>
       </svg>
-
+      </div>
     </div>
     )
   }
--- src/Components/IceCream.js
+++ src/Components/IceCream.js
@@ -15,16 +15,13 @@ const {palette, flavour, price, nuts} = this.props;
 
 var style = {
      backgroundColor: palette,
-     color: 'white',
-     margin: "0px",
-     height: '30px'
    };
 
     return (
-      <div style={style}>
-        Im delicous {flavour} flavour with {nuts} and i cost {price} kr
-        <button className="btn btn-primary" onClick={this.deleteIceCream.bind(this)}>Cancel order</button>
-      </div>
+      <div className="wrapper" style={style}>
+        <div className="textdiv"><p>Im delicous {flavour} flavour with {nuts} and i cost {price} kr</p></div>
+        <div className="knappdiv"><button onClick={this.deleteIceCream.bind(this)}>Cancel order</button></div>
+    </div>
     )
 
   }
